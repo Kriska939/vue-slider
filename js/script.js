71d343114003fb544c39ca33a06df5c244dd7dae
Kriska939 <cristinacorrado93@gmail.com>
2021-07-28 15:52:24 +0200

    added new Vue object

--- js/script.js
+++ js/script.js
@@ -6,4 +6,20 @@
 Rifare l'esercizio dello slider come fatto assieme in classe.
 */
 
+Vue.config.devtools = true;
 
+const app = new Vue({
+    el: "#app",
+    data: {
+        images: [
+            "./img/image1.jpg",
+            "./img/image2.jpg",
+            "./img/image3.jpg",
+            "./img/image4.jpg",
+        ],
+    },
+    methods: {
+
+    },
+
+});
